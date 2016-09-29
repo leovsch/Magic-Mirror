6e10f38498547c88b895d9ecde0d4c5d088dce23
Leo van Schooten <Leo van Schooten>
2016-09-29 20:51:23 +0200

    add weather icons :) and styling

--- angular/MagicMirrorApp.js
+++ angular/MagicMirrorApp.js
@@ -2,7 +2,7 @@ var App = angular.module('MagicMirrorApp', []);
 
 App.constant('config', {
 	weatherApiKey: '{ApiKey}',
-	weatherCityID: '2746301',
+	weatherCityID: '2746301', // Tilburg
 	newsFeedRssUrl: 'http://www.nu.nl/rss/Algemeen',
 	getNewsInterval: 3600000, // miliseconds (1 hour)
     newsScrollInterval: 30000 // miliseconds (30 seconds)
--- angular/controller/WeatherCtrl.js
+++ angular/controller/WeatherCtrl.js
@@ -1,11 +1,10 @@
 App.controller("WeatherCtrl", ['$scope','WeatherService', '$filter', function ($scope,WeatherService, $filter) {
 	WeatherService.getWeather().then(function(weather) {
-		console.log(weather);
 		setWeather(weather);
 	});
 
 	setWeather = function(weather) {
+		console.log(weather);
 		$scope.forecast = $filter('forecast')(weather.list);
-		console.log(forecast);
 	}
 }]);
