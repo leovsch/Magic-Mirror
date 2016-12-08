224804ba3bd062fea5a56c08fa96af8840d10a25
Leo van Schooten <Leo van Schooten>
2016-12-08 18:29:35 +0100

    code cleanup

--- Python/motiondetection.py
+++ Python/motiondetection.py
@@ -1,4 +1,5 @@
 from gpiozero import MotionSensor
+from gpiozero import DigitalInputDevice
 import time
 import subprocess
 
@@ -6,16 +7,23 @@ pir = MotionSensor(4, queue_len=15, threshold=0.90)
 count = 0
 monitor_on = True
 
-while True:
+def motion_detection():
     if pir.motion_detected:
-        print("Motion detected!", count)
         count = count + 1
         if monitor_on == False:
+        	print("Turning monitor on")
         	subprocess.call("/opt/vc/bin/tvservice -p", shell=True)
         	monitor_on = True        
         subprocess.call("omxplayer /home/pi/Desktop/01\ Mumford\ \&\ Sons\ -\ Babel.mp3", shell=True)
     else:
     	count = count + 1
     	if monitor_on == True:
+    		print("turning monitor off")
     		subprocess.call("/opt/vc/bin/tvservice -o", shell=True)
     		monitor_on = False
+
+def remote_input():
+	print(remote.value)
+	
+while True:
+	remote_input()
