90cea849cf26e60d195f530196062a19f21c79c6
Leo van Schooten <Leo van Schooten>
2016-12-09 20:45:20 +0100

    add python code for remote control and starts playing random music in music folder when there is motion

--- Python/motiondetection.py
+++ Python/motiondetection.py
@@ -2,28 +2,59 @@ from gpiozero import MotionSensor
 from gpiozero import DigitalInputDevice
 import time
 import subprocess
+import lirc
+import random
+import os
 
+# constant variables
+music_dir = "/home/pi/Music/"
+
+# collection variables
+playlist = []
+
+# init the motion sensor
 pir = MotionSensor(4, queue_len=15, threshold=0.90)
-count = 0
 monitor_on = True
 
-def motion_detection():
-    if pir.motion_detected:
-        count = count + 1
-        if monitor_on == False:
-        	print("Turning monitor on")
-        	subprocess.call("/opt/vc/bin/tvservice -p", shell=True)
-        	monitor_on = True        
-        subprocess.call("omxplayer /home/pi/Desktop/01\ Mumford\ \&\ Sons\ -\ Babel.mp3", shell=True)
-    else:
-    	count = count + 1
-    	if monitor_on == True:
-    		print("turning monitor off")
-    		subprocess.call("/opt/vc/bin/tvservice -o", shell=True)
-    		monitor_on = False
+# init the remote
+sockid = lirc.init("slidepuzzle", blocking = False)
+lirc.load_config_file("/etc/lirc/lircrc")
 
+# init the music playlist
+for filename in os.listdir(positives_dir):
+	playlist.append(filename)
+
+# function to handle motion detection
+def motion_detection():
+	global monitor_on
+	global playlist
+
+	if pir.motion_detected:
+		if monitor_on == False:
+			print("Turning monitor on")
+			os.system("/opt/vc/bin/tvservice -p")
+			monitor_on = True
+
+		# pick number to play
+		randint = random.randint(0, len(playlist) - 1)        
+		subprocess.call("omxplayer " + music_dir + playlist[randint], shell=True)
+	else:
+		if monitor_on == True:
+			print("turning monitor off")
+			os.system("/opt/vc/bin/tvservice -o")
+			monitor_on = False
+
+# function to handle remote input
 def remote_input():
-	print(remote.value)
+	# read the input value and check if there is a button press
+	input_value = lirc.nextcode()
+	if len(input_value) >= 1:
+		# read the input string
+		intput_string = input_value[0]
+		if intput_string == 'right':
+			subprocess.call("omxplayer /home/pi/Desktop/01\ Mumford\ \&\ Sons\ -\ Babel.mp3", shell=True)
+
 	
 while True:
 	remote_input()
+	motion_detection()
