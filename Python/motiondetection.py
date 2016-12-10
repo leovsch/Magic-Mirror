3b971ae1f2ab40bc82f4336e3b93fb23506e12eb
Leo van Schooten <Leo van Schooten>
2016-12-10 17:50:52 +0100

    minor python change

--- Python/motiondetection.py
+++ Python/motiondetection.py
@@ -1,6 +1,5 @@
 from gpiozero import MotionSensor
 from gpiozero import DigitalInputDevice
-import time
 import subprocess
 import lirc
 import random
@@ -21,7 +20,7 @@ sockid = lirc.init("slidepuzzle", blocking = False)
 lirc.load_config_file("/etc/lirc/lircrc")
 
 # init the music playlist
-for filename in os.listdir(positives_dir):
+for filename in os.listdir(music_dir):
 	playlist.append(filename)
 
 # function to handle motion detection
@@ -32,27 +31,27 @@ def motion_detection():
 	if pir.motion_detected:
 		if monitor_on == False:
 			print("Turning monitor on")
-			os.system("/opt/vc/bin/tvservice -p")
+			subprocess.call("/opt/vc/bin/tvservice -p", shell=True)
 			monitor_on = True
-
-		# pick number to play
-		randint = random.randint(0, len(playlist) - 1)        
-		subprocess.call("omxplayer " + music_dir + playlist[randint], shell=True)
 	else:
 		if monitor_on == True:
 			print("turning monitor off")
-			os.system("/opt/vc/bin/tvservice -o")
+			subprocess.call("/opt/vc/bin/tvservice -o", shell=True)
 			monitor_on = False
 
 # function to handle remote input
 def remote_input():
+	global music_dir
+	global playlist
 	# read the input value and check if there is a button press
 	input_value = lirc.nextcode()
 	if len(input_value) >= 1:
 		# read the input string
 		intput_string = input_value[0]
 		if intput_string == 'right':
-			subprocess.call("omxplayer /home/pi/Desktop/01\ Mumford\ \&\ Sons\ -\ Babel.mp3", shell=True)
+			# pick number to play
+			randint = random.randint(0, len(playlist) - 1) 
+			subprocess.call("omxplayer " + music_dir + playlist[randint], shell=True)
 
 	
 while True:
