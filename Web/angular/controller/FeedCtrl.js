c9e179718eacce5c1a04af228eb9f5b3cc2972e2
Leo van Schooten <Leo van Schooten>
2017-03-08 00:06:17 +0100

    add more logic navigation when scrolling through newsfeed

--- Web/angular/controller/FeedCtrl.js
+++ Web/angular/controller/FeedCtrl.js
@@ -4,12 +4,30 @@ var newsfeedindex = 0;
 
 App.controller("FeedCtrl", ['$scope','FeedService', '$interval', 'config', '$rootScope', function ($scope,Feed,$interval,config,$rootScope) {
 
+    $scope.ShowChevronLeft = false;
+    $scope.ShowChevronRight = true;
+
     getnews = function (url) {
-            Feed.parseFeed(url).then(function(res) {
-                feeds = res.data.items;
-                $scope.feed = feeds[0];
-                feedcount = 1;
+        Feed.parseFeed(url).then(function(res) {
+            feeds = res.data.items;
+            $scope.feed = feeds[0];
+            feedcount = 1;
         });
+    }
+
+    showHideChevron = function() {
+        if(newsfeedindex == 0) {
+            $scope.ShowChevronLeft = false;
+            $scope.ShowChevronRight = true;
+        }
+        if(newsfeedindex == config.newsFeedRssUrl.length - 1) {
+            $scope.ShowChevronLeft = true;
+            $scope.ShowChevronRight = false;
+        }
+        if(newsfeedindex > 0 && newsfeedindex < config.newsFeedRssUrl.length - 1) {
+            $scope.ShowChevronLeft = true;
+            $scope.ShowChevronRight = true;
+        }
     }
 
     getnews(config.newsFeedRssUrl[newsfeedindex]);
@@ -31,12 +49,14 @@ App.controller("FeedCtrl", ['$scope','FeedService', '$interval', 'config', '$roo
             if(newsfeedindex < config.newsFeedRssUrl.length - 1) {
                 newsfeedindex++;
                 getnews(config.newsFeedRssUrl[newsfeedindex]);
+                showHideChevron();
             }
         }
         if(data == "left") {
             if(newsfeedindex > 0){
                 newsfeedindex--;
                 getnews(config.newsFeedRssUrl[newsfeedindex]);
+                showHideChevron();
             }
         }
     });
